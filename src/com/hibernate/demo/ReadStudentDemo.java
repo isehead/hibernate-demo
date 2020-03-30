a1b2c2553ac19432aebc06dc09bb00a3f52e0b08
isehead <isehead@gmail.com>
2020-03-30 16:48:59 +0500

    Create and read a DB record by ID

---
+++ src/com/hibernate/demo/ReadStudentDemo.java
@@ -0,0 +1,57 @@
+package com.hibernate.demo;
+
+import com.hibernate.demo.entity.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class ReadStudentDemo {
+
+    public static void main(String[] args) {
+
+        // create session factory
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+
+        // create session
+        Session session = factory.getCurrentSession();
+
+        try {
+            // create a student object
+            System.out.println("Creating new student object...");
+            Student tempStudent = new Student("Daffy", "Duck", "daffyduck@gmail.com");
+
+            // start a transaction
+            session.beginTransaction();
+
+            // save the student object
+            System.out.println("Saving the student");
+            System.out.println(tempStudent);
+            session.save(tempStudent);
+
+            // commit transaction
+            session.getTransaction().commit();
+            System.out.println("Saved student generated id: " + tempStudent.getId());
+
+            // get a new session and start a transaction
+            session = factory.getCurrentSession();
+            session.beginTransaction();
+
+            // retrieve student based on id: primary key
+            System.out.println("\nGetting student with id: " + tempStudent.getId());
+            Student myStudent = session.get(Student.class, tempStudent.getId());
+            System.out.println("Get complete: " + myStudent);
+
+            //commit the transaction
+            session.getTransaction().commit();
+
+            System.out.println("Done!");
+
+        } finally {
+            factory.close();
+        }
+
+    }
+}
