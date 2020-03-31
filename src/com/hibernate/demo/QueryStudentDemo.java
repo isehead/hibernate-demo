ab72818cbfc1a2e620fb1c6fc793bfd05f9d4ba4
isehead <isehead@gmail.com>
2020-03-31 11:41:32 +0500

    Queries with Hibernate - OR, LIKE. Log4j configured for debugging

--- src/com/hibernate/demo/QueryStudentDemo.java
+++ src/com/hibernate/demo/QueryStudentDemo.java
@@ -7,6 +7,8 @@ import org.hibernate.cfg.Configuration;
 
 import java.util.List;
 
+import static com.hibernate.demo.TextColors.*;
+
 public class QueryStudentDemo {
 
     public static void main(String[] args) {
@@ -27,17 +29,33 @@ public class QueryStudentDemo {
 
             // query students
             List<Student> theStudents = session.createQuery("from Student").getResultList();
+            System.out.println(ANSI_BLUE + "\nList of all students:" + ANSI_RESET);
             displayStudents(theStudents);
 
-            // query students: lastname = 'Doe'
-            theStudents = session.createQuery("from Student s where s.lastName = 'Doe'").getResultList();
+            // query students: lastName = 'Doe'
+            theStudents = session.createQuery("from Student s where s.lastName = 'Duck'").getResultList();
+            System.out.println(ANSI_BLUE + "\nStudents with lastName 'Duck':" + ANSI_RESET);
+
+            // display the students
+            displayStudents(theStudents);
+
+            // query students: lastName = 'Doe' OR firstName = 'Daffy'
+            theStudents = session.createQuery("from Student s where s.lastName = 'Doe' or firstName = 'Daffy'").getResultList();
+            System.out.println(ANSI_BLUE + "\nStudents with lastName 'Doe' OR firstName 'Daffy':" + ANSI_RESET);
+
+            // display the students
+            displayStudents(theStudents);
+
+            // query students: email LIKE %gmail.com
+            theStudents = session.createQuery("from Student s where s.email LIKE '%@gmail.com'").getResultList();
+            System.out.println(ANSI_BLUE + "\nStudents with email on gmail:" + ANSI_RESET);
 
             // display the students
             displayStudents(theStudents);
 
             // commit transaction
             session.getTransaction().commit();
-            System.out.println("Done!");
+            System.out.println("\nDone!\n");
 
         } finally {
             factory.close();
