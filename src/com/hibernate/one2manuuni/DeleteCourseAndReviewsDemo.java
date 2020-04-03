0408a53c70357ffbd0b94cfa0f42c18ce5d2e5e5
isehead <isehead@gmail.com>
2020-04-03 17:31:05 +0500

    One@Many uni-directional: get and delete functions released

--- src/com/hibernate/one2manuuni/DeleteCourseAndReviewsDemo.java
+++ src/com/hibernate/one2manuuni/DeleteCourseAndReviewsDemo.java
@@ -3,39 +3,41 @@ package com.hibernate.one2manuuni;
 import com.hibernate.one2manuuni.entity.Course;
 import com.hibernate.one2manuuni.entity.Instructor;
 import com.hibernate.one2manuuni.entity.InstructorDetail;
+import com.hibernate.one2manuuni.entity.Review;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class CreateInstructorDemo {
+public class DeleteCourseAndReviewsDemo {
 
     public static void main(String[] args) {
 
         // create session factory
         SessionFactory factory = new Configuration()
-                .configure("hibernate-1toMany.cfg.xml")
+                .configure("hibernate-1toManyUni.cfg.xml")
                 .addAnnotatedClass(Instructor.class)
                 .addAnnotatedClass(InstructorDetail.class)
                 .addAnnotatedClass(Course.class)
+                .addAnnotatedClass(Review.class)
                 .buildSessionFactory();
 
         // create session
         Session session = factory.getCurrentSession();
 
         try {
-            // create the objects
-            Instructor tempInstructor = new Instructor("Susan", "Public", "susan.public@l2code.com");
-            InstructorDetail tempInstructorDetail = new InstructorDetail("psports", "football");
-
-            // associate the objects
-            tempInstructor.setInstructorDetail(tempInstructorDetail);
-
             // start a transaction
             session.beginTransaction();
 
-            // save the instructor, CascadeType.ALL includes saving associated object
-            System.out.println("Saving instructor: " + tempInstructor);
-            session.save(tempInstructor);
+            // get the course
+            int theId = 10;
+            Course course = session.get(Course.class, theId);
+
+            // print the course with reviews
+            System.out.println(course);
+            System.out.println(course.getReviews());
+
+            // delete the course
+            session.delete(course);
 
             // commit transaction
             session.getTransaction().commit();
--- src/com/hibernate/one2manuuni/GetCourseAndReviewsDemo.java
+++ src/com/hibernate/one2manuuni/GetCourseAndReviewsDemo.java
@@ -3,20 +3,22 @@ package com.hibernate.one2manuuni;
 import com.hibernate.one2manuuni.entity.Course;
 import com.hibernate.one2manuuni.entity.Instructor;
 import com.hibernate.one2manuuni.entity.InstructorDetail;
+import com.hibernate.one2manuuni.entity.Review;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class GetInstructorCoursesDemo {
+public class GetCourseAndReviewsDemo {
 
     public static void main(String[] args) {
 
         // create session factory
         SessionFactory factory = new Configuration()
-                .configure("hibernate-1toMany.cfg.xml")
+                .configure("hibernate-1toManyUni.cfg.xml")
                 .addAnnotatedClass(Instructor.class)
                 .addAnnotatedClass(InstructorDetail.class)
                 .addAnnotatedClass(Course.class)
+                .addAnnotatedClass(Review.class)
                 .buildSessionFactory();
 
         // create session
@@ -26,13 +28,13 @@ public class GetInstructorCoursesDemo {
             // start a transaction
             session.beginTransaction();
 
-            // get the instructor from the database
-            int theId = 1;
-            Instructor instructor = session.get(Instructor.class, theId);
+            // get the course
+            int theId = 10;
+            Course course = session.get(Course.class, theId);
 
-            // show courses list
-            System.out.println("Requested instructor is: " + instructor);
-            System.out.println(instructor.getCourseList());
+            // print the course with reviews
+            System.out.println(course);
+            System.out.println(course.getReviews());
 
             // commit transaction
             session.getTransaction().commit();
