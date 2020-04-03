574d1d2749d3af1d31b45bec2053739cdb67f52e
isehead <isehead@gmail.com>
2020-04-03 22:31:45 +0500

    @ManyToMany: prepwork - new db schema, modified entities

---
+++ src/com/hibernate/many2many/CreateCourseAndReviewsDemo.java
@@ -0,0 +1,55 @@
+package com.hibernate.many2many;
+
+import com.hibernate.one2manuuni.entity.Course;
+import com.hibernate.one2manuuni.entity.Instructor;
+import com.hibernate.one2manuuni.entity.InstructorDetail;
+import com.hibernate.one2manuuni.entity.Review;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class CreateCourseAndReviewsDemo {
+
+    public static void main(String[] args) {
+
+        // create session factory
+        SessionFactory factory = new Configuration()
+                .configure("hibernate-1toManyUni.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .addAnnotatedClass(Course.class)
+                .addAnnotatedClass(Review.class)
+                .buildSessionFactory();
+
+        // create session
+        Session session = factory.getCurrentSession();
+
+        try {
+            // start a transaction
+            session.beginTransaction();
+
+            // create a course
+            Course course = new Course("Pacman - How to score 1b points");
+
+            // add some reviews
+            course.addReview(new Review("Great course!"));
+            course.addReview(new Review("Worth every penny"));
+            course.addReview(new Review("Better than nothing..."));
+            course.addReview(new Review("Where is my moneyback???"));
+
+            // save the course and let cascading save everything else
+            System.out.println("Saving the course:" + course);
+            System.out.println(course.getReviews());
+            session.save(course);
+
+            // commit transaction
+            session.getTransaction().commit();
+            System.out.println("Done!");
+
+        } finally {
+            session.close();
+            factory.close();
+        }
+
+    }
+}
---
+++ src/com/hibernate/many2many/entity/Course.java
@@ -0,0 +1,106 @@
+package com.hibernate.many2many.entity;
+
+import com.hibernate.many2many.entity.Instructor;
+import com.hibernate.many2many.entity.Review;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "course")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "title")
+    private String title;
+
+    @ManyToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinColumn(name = "instructor_id")
+    private Instructor instructor;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "course_id")
+    private List<Review> reviews;
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade =
+            {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinTable(name = "course_student", joinColumns = @JoinColumn(name = "course_id"),
+            inverseJoinColumns = @JoinColumn(name = "student_id"))
+    private List<Student> students;
+
+    public Course(String title) {
+        this.title = title;
+    }
+
+    public Course() {
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                '}';
+    }
+
+    // convenience method to add a review
+    public void addReview(Review review) {
+        if (reviews == null) {
+            reviews = new ArrayList<>();
+        }
+        reviews.add(review);
+    }
+
+    // convenience method to add a single student to a course
+    public void addStudentToCourse(Student student) {
+        if (students == null) {
+            students = new ArrayList<>();
+        }
+        students.add(student);
+    }
+}
